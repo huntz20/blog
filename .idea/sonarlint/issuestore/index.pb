beb5329c85901496fbf9114c806dcf413b275536
huntz20 <huntzrahmadi@gmail.com>
2022-03-17 15:54:01 +0700

    adding index

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,7 +6,5 @@ A
 postcss.config.js,3/f/3f70684581fd16ab3be77ca959e0138ef3ee8bfb
 9
 	yarn.lock,5/3/53585a096fe3a3b96321113856ae62602995a33b
-M
-public/icon/Twitter White.svg,5/f/5f931687cf32e1b2290ced6a7db1c11f5e4d4feb
-<
-package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+L
+public/icon/twitter-logo.svg,d/9/d91c043d860d502e90a9bf7efab6046508cf1129
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./index.vue",
+  ],
+  theme: {
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+    fontFamily: {
+      'cursive': ['Patrick Hand'],
+      'sans-serif': ['Poppins'],
+      'serif': ['Roboto Slab']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
